dfe2264089397ebbe24272dfe6bdf5946ceab9b7
st148627 <stura.simone@educ.di.unito.it>
2018-07-14 17:46:26 +0200

    asdf

---
+++ src/FilmFestival.java
@@ -0,0 +1,206 @@
+import java.util.*;
+
+public class FilmFestival {
+	
+	private TreeSet<Screening> shows = new TreeSet<>((Screening s1, Screening s2) -> s1.compareTo(s2));
+	private ArrayList<Movie> movies = new ArrayList<>();
+	private HashMap<Movie,ArrayList<Screening>> screens = new HashMap<>();
+	public Graph<Screening> conflicts = new Graph<>();
+	
+	public FilmFestival() {}
+	
+	public boolean addScreen(Screening s) {
+		boolean ctrl = shows.add(s);
+		if(ctrl) {
+			Movie m = s.getM();
+			if(!(movies.contains(m))) {
+				movies.add(m);
+				screens.put(m, new ArrayList<>(3));
+			}
+			screens.get(m).add(s);
+			for(Screening screen : shows)
+				if(!(screen.getM().equals(m)) && Screening.isConflict(s, screen))
+					conflicts.addEdge(screen, s);
+		}
+		return ctrl;
+	}
+	
+	public Collection<Screening> getShows() {
+		return shows;
+	}
+	
+	public Collection<Movie> getMovies() {
+		return movies;
+	}
+	
+	public Map<Movie,ArrayList<Screening>> getScreens() {
+		return screens;
+	}
+	
+	public List<Movie> conflicting(Movie m) {
+		if(m == null) return null;
+		ArrayList<Movie> confl = new ArrayList<>();
+		ArrayList<Screening> actualScreens = screens.get(m);
+		List<List<Screening>> conflScreens = new ArrayList<List<Screening>>(actualScreens.size());
+		for(Screening screen : actualScreens)
+			conflScreens.add(conflicts(screen).notObscurated());
+		
+		return confl;
+	}
+	
+	public Graph<Screening> conflicts(Screening screen) {
+		Graph<Screening> g = new Graph<>();
+		return conflicts(screen, g, null, new ArrayList<>());
+	}
+	
+	private Graph<Screening> conflicts(Screening screen, Graph<Screening> g, 
+			Screening father, List<Screening> visited) {
+		visited.add(screen);
+		for(Screening s : shows) {
+			Calendar calS = Calendar.getInstance();
+			Calendar calE = Calendar.getInstance();
+			calS.setTime(s.getStartTime());
+			calE.setTime(screen.getEndTime());
+			if(calS.get(Calendar.DAY_OF_MONTH) > calE.get(Calendar.DAY_OF_MONTH) 
+					|| calS.get(Calendar.MONTH) > calE.get(Calendar.MONTH)
+					|| calS.get(Calendar.YEAR) > calE.get(Calendar.YEAR))
+				break;
+			if(!(s.getM().equals(screen.getM()) || (father != null && s.getM().equals(father.getM())))
+					&& Screening.isConflict(screen, s)) {
+				g.addEdge(screen, s);
+				if(visited.contains(s)) continue;
+				conflicts(s, g, screen, visited);
+			}
+		}
+		return g;
+	}
+	
+	public static void main(String[] args) {
+		FilmFestival tff = new FilmFestival();
+		Movie m = new Movie("God Bless The Child", 2015, 92);
+		Calendar cal = Calendar.getInstance();
+		cal.set(2015,10,21,17,0);
+		Screening scr = new Screening(m, cal.getTime());
+		tff.addScreen(scr);
+		cal.set(2015,10,22,11, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,19, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("Sufragette", 2015, 100);
+		cal.set(2015,10,21,14, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,17, 15);
+		scr = new Screening(m, cal.getTime());
+		tff.addScreen(scr);
+		
+		m = new Movie("Idealisten", 2015, 114);
+		cal.set(2015,10,22,16, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,12, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,24,19, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("La Patota", 2015, 103);
+		cal.set(2015,10,22,22, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,9, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,24,9, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("Lo Scambio", 2015, 93);
+		cal.set(2015,10,23,17, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,24,19, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("Sopladora De Hojas", 2015, 96);
+		cal.set(2015,10,23,20, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,24,9, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("High-Rise", 2015, 112);
+		cal.set(2015,10,23,22, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,24,14, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("Me & Earl & The Dying Girl", 2015, 100);
+		cal.set(2015,10,22,22, 15);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,14, 15);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("February", 2015, 93);
+		cal.set(2015,10,22,9, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,22, 15);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("Mia Madre Fa L'Attrice", 2015, 78);
+		cal.set(2015,10,21,19, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,22,9, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,11, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("The Girl In The Photographs", 2015, 95);
+		cal.set(2015,10,21,22, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,22,9, 0);
+		Screening ch2 = new Screening(m, cal.getTime());
+		tff.addScreen(ch2);
+		cal.set(2015,10,24,22, 15);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+		m = new Movie("The Quiet Earth", 2015, 91);
+		cal.set(2015,10,21,14, 45);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,22,9, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,17, 15);
+		Screening ch1 = new Screening(m, cal.getTime());
+		tff.addScreen(ch1);
+		
+		m = new Movie("Nusty Baby", 2015, 100);
+		cal.set(2015,10,21,22, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,22,14, 30);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		cal.set(2015,10,23,9, 0);
+		tff.addScreen(new Screening(m, cal.getTime()));
+		
+//		for(Screening screen : tff.getShows())
+//			System.out.println(screen + " " + screen.getM());
+		/*
+		for(Movie mov : tff.getMovies()) {
+			System.out.println(mov);
+			for(Screening screen : tff.getScreens().get(mov))
+				System.out.println("\t" + screen);
+			System.out.println();
+		}
+		*/
+		//tff.conflicts.choose(ch1);
+		//tff.conflicts.choose(ch2);
+		List<Screening> list = tff.conflicts.cyclic();
+		if(list == null) return;
+		for(Screening screen : list)
+			System.out.println("\t" + screen + " " + screen.getM());
+/*		for(Screening scree : tff.getShows()) {
+			System.out.println(scree + " " + scree.getM());
+			Graph<Screening> g = tff.conflicts(scree);
+			List<Screening> l = g.cyclic();
+			if(l != null) {
+				System.out.println("CICLO:");
+				for(Screening screen : g.notObscurated())
+					System.out.println("\t" + screen + " " + screen.getM());
+			}
+		}
+		*/
+	}
+
+}
---
+++ src/Screening.java
@@ -0,0 +1,133 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Screening {
+	private Movie m;
+	private Date startTime;
+	private Date endTime;
+	private String cinema = "";
+	
+	private static SimpleDateFormat df = new SimpleDateFormat("EEE dd/MM HH:mm");
+	private static SimpleDateFormat dfHour = new SimpleDateFormat("HH:mm");
+	
+	public Screening(Movie m, Date startTime) {
+		this.m = m;
+		setStartTime(startTime);
+	}
+	
+	public Screening(Movie m, Date startTime, String cinema) {
+		this.m = m;
+		this.cinema = cinema;
+		setStartTime(startTime);
+	}
+	
+	public void setMovie(Movie m) {
+		this.m = m;
+		setStartTime(this.startTime);
+	}
+	
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+		endTime = new Date(startTime.getTime() + 60000 * m.getRuntime());
+	}
+	
+	public void setCinema(String cinema) {
+		this.cinema = cinema;
+	}
+	
+	public Movie getM() {
+		return m;
+	}
+	
+	public Date getStartTime() {
+		return startTime;
+	}
+	
+	public Date getEndTime() {
+		return endTime;
+	}
+	
+	public String getCinema() {
+		return cinema;
+	}
+	
+	public String toString() {
+		String s = df.format(startTime) + " - ";
+		Calendar calS = Calendar.getInstance();
+		Calendar calE = Calendar.getInstance();
+		calS.setTime(startTime);
+		calE.setTime(endTime);
+		if(calS.get(Calendar.DAY_OF_MONTH) == calE.get(Calendar.DAY_OF_MONTH) 
+				&& calS.get(Calendar.MONTH) == calE.get(Calendar.MONTH))
+			s += dfHour.format(endTime);
+		else
+			s += df.format(endTime);
+		return s;
+	}
+	
+	public boolean equals(Object o) {
+		if(!(o instanceof Screening)) return false;
+		Screening s = (Screening) o;
+		return this.m.equals(s.m) && this.startTime.equals(s.startTime) 
+				&& this.cinema.equals(s.cinema);
+	}
+	
+	public int compareTo(Screening s) {
+		int cmp = 0;
+		if(startTime.compareTo(s.startTime) == 0) {
+			if(endTime.compareTo(s.endTime) == 0) {
+				if(m.compareTo(s.m) == 0)
+					cmp = cinema.compareTo(s.cinema);
+				else
+					cmp = m.compareTo(s.m);
+			} else
+				cmp = endTime.compareTo(s.endTime);
+		} else
+			cmp = startTime.compareTo(s.startTime);
+		return cmp;
+	}
+	
+	public static boolean isConflict(Screening s1, Screening s2) {
+		return isConflict(s1, s2, 0);
+	}
+	
+	public static boolean isConflict(Screening s1, Screening s2, int minutesBtw) {
+		long distance = s2.startTime.getTime() - s1.endTime.getTime();
+		if(distance/60000 > minutesBtw) return false;
+		distance = s1.startTime.getTime() - s2.endTime.getTime();
+		if(distance/60000 > minutesBtw) return false;
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Movie m = new Movie("La La Land", 2016, 128);
+		Calendar cal = Calendar.getInstance();
+		cal.set(2018,6,5,21,45);
+		Screening s1 = new Screening(m, cal.getTime());
+		cal.set(2018,6,5,21,45);
+		Screening s2 = new Screening(m, cal.getTime());
+		cal.set(2018,6,6,20,45);
+		Screening s3 = new Screening(m, cal.getTime());
+		System.out.println(s1 + " " + s1.getM());
+		System.out.println(s2 + " " + s2.getM());
+		System.out.println(s3 + " " + s3.getM());
+		System.out.println("s1 equals s2 => " + s1.equals(s2));
+		System.out.println("s1 equals s3 => " + s1.equals(s3));
+		System.out.println("s1 compareTo s2 => " + s1.compareTo(s2));
+		System.out.println("s1 compareTo s3 => " + s1.compareTo(s3));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
